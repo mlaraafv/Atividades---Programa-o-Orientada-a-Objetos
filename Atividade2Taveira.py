1f30d70740d3a5b995c4ec80149e5a0cc577439e
mlaraafv <72415921+mlaraafv@users.noreply.github.com>
2020-11-08 19:51:27 -0300

    Add files via upload

---
+++ Atividade2Taveira.py
@@ -0,0 +1,25 @@
+def decimal(b):
+    print('in dec', b,  end=" ")
+
+
+def octal(b):
+    print('in oct=', oct(b), end=" ")
+
+
+def hexadecimal(b):
+    print('in hex=', hex(b), end=" ")
+
+
+def binario(b):
+    print('in bin=', bin(b), end=" ")
+
+
+def tabela():
+    a = 0
+    while a < 226:
+        decimal(a), octal(a), hexadecimal(a), binario(a)
+        print("\n")
+        a += 1
+
+
+tabela()
