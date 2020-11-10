470f0dd0d2c1ded4aac667f834ebc189570cd9b9
mlaraafv <72415921+mlaraafv@users.noreply.github.com>
2020-11-10 15:45:06 -0300

    Update Atividade2Taveira.py

--- Atividade2Taveira.py
+++ Atividade2Taveira.py
@@ -1,25 +1,27 @@
 def printDecimal(b):
-    print('in dec', b,  end=" ")
+    print('\t', b,  end=" ")
 
 
 def printOctal(b):
-    print('in oct=', oct(b), end=" ")
+    print('\t', oct(b), end=" ")
 
 
 def printHexadecimal(b):
-    print('in hex=', hex(b), end=" ")
+    print('\t', hex(b), end=" ")
 
 
 def printBinario(b):
-    print('in bin=', bin(b), end=" ")
+    print('\t  ', bin(b), end=" ")
 
 
 def imprimirTabela():
+    print("Decimal Octal Hexadecimal Binario")
+    print("------- ----- ----------- -------- ")
     a = 0
     while a < 256:
         printDecimal(a), printOctal(a), printHexadecimal(a), printBinario(a)
         print("\n")
-        print("------------------------------------------------------- ")
+
         a += 1
 
 
